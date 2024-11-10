7bc6468d821aef7b4bced0dce91c00d5ad8f7483
SaraMoh0103 <saramohamed0103@gmail.com>
2024-11-10 13:37:40 +0200

    Updating the parking lot class

--- src/main/java/parking/os/ParkingLot.java
+++ src/main/java/parking/os/ParkingLot.java
@@ -4,23 +4,24 @@ public class ParkingLot {
     private int numberOfSpots;
     private final Semaphore semaphore;
     public ParkingLot(Semaphore semaphore) {
-        int numberOfSpots = 4;
+        this.numberOfSpots = 4;
         this.semaphore = new Semaphore(numberOfSpots);
     }
 
     public boolean CheckAvailability() throws InterruptedException {
         if (semaphore.availablePermits() > 0) {
             semaphore.acquire();
-            System.out.print("Car has been parked successfully");
             return true;
         } else {
-            System.out.println("No spots available");
+           semaphore.acquire();
             return false;
         }
     }
     public void leave(){
         semaphore.release();
-        System.out.println("Car has left the parking lot");
+    }
+    public int getNumberOfSpots(){
+        return semaphore.availablePermits();
     }
 
 }
